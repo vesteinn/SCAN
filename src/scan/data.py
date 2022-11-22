81bcb9524079c6bd6753420b46108d207ad0ee60
Vésteinn Snæbjarnarson <vesteinnsnaebjarnarson@gmail.com>
2022-11-22 21:14:00 +0100

    Added SCAN dataset and dataset submodule

---
+++ src/scan/data.py
@@ -0,0 +1,90 @@
+import torch
+
+
+def parse_action_command(line):
+    command, action = line.strip().split(" OUT: ")
+    command = command[3:]  # Remove "IN: "
+    return command, action
+
+
+def generate_scan_dictionary(full_dataset, add_bos=False, add_eos=False):
+    #
+    # Example (split on separator):
+    # IN: walk opposite right thrice after run opposite right
+    # OUT: I_TURN_RIGHT I_TURN_RIGHT I_RUN I_TURN_RIGHT I_TURN_RIGHT I_WALK I_TURN_RIGHT I_TURN_RIGHT I_WALK I_TURN_RIGHT I_TURN_RIGHT I_WALK
+    # 
+    command_dict = dict()
+    action_dict = dict()
+
+    commands = set()
+    actions = set()
+
+    with open(full_dataset) as infile:
+        for line in infile.readlines():
+            command, action = parse_action_command(line)
+            commands.update(set(command.split()))
+            actions.update(set(action.split()))
+    
+    # Sorted needed for deterministic id's
+    for idx, action in enumerate(sorted(actions)):
+        action_dict[action] = idx
+    for idx, command in enumerate(sorted(commands)):
+        command_dict[command] = idx
+    
+    action_dict["PAD"] = -1
+    command_dict["PAD"] = -1
+
+    if add_bos:
+        action_dict["BOS"] = len(action_dict)
+        command_dict["BOS"] = len(command_dict)
+    if add_eos:
+        action_dict["EOS"] = len(action_dict)
+        command_dict["EOS"] = len(command_dict)
+
+    return command_dict, action_dict
+
+
+class SCANDataset(torch.utils.data.Dataset):
+    commands = []
+    actions = []
+
+    def __init__(self, data, src_dict, tgt_dict):
+        self.src_dict = src_dict
+        self.tgt_dict = tgt_dict
+        self.load(data)
+
+    def __len__(self):
+        return len(self.commands)
+
+    def __get__(self, idx):
+        return (self.commands[idx], self.actions[idx])
+
+    def encode(self, text, dictionary):
+        return [dictionary[a] for a in text.strip().split()]
+
+    def encode_command(self, command):
+        return self.encode(command, self.src_dict)
+
+    def encode_action(self, action):
+        return self.encode(action, self.tgt_dict)
+
+    def load(self, data):
+        with open(data) as infile:
+            for line in infile.readlines():
+                command, action = parse_action_command(line)
+                self.commands.append(self.encode_command(command))
+                self.actions.append(self.encode_action(action))
+         
+
+if __name__ == "__main__":
+    # Simple test for ScanDataset, assumes submodule with data
+    # has been loaded.
+    tasks = "../../data/SCAN/tasks.txt"
+    src_dict, tgt_dict = generate_scan_dictionary(tasks)
+    assert len(src_dict) > 0
+    assert len(tgt_dict) > 0
+    print(f"Dictionaries lodaded, {len(src_dict)} in src_dict, {len(tgt_dict)} in tgt_dict.")
+
+    dataset = SCANDataset(tasks, src_dict, tgt_dict)
+    assert len(dataset) > 2000
+    print(f"Dataset loaded with {len(dataset)} records.")
