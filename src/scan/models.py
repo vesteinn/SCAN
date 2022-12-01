42ced3cfac41c4bad4d801e1a1bfe8d4fa251a92
Vésteinn Snæbjarnarson <vesteinnsnaebjarnarson@gmail.com>
2022-12-01 21:24:37 +0100

    Setup base models

--- src/scan/models.py
+++ src/scan/models.py
@@ -6,47 +6,64 @@ import torch.optim as optim
 from data import generate_scan_dictionary, SCANDataset
 
 
-class LSTMEncoder(nn.Module):
+#
+# TODO: The dropout used here is not same as in paper!
+#       DOES NOT WORK WITH SINGLE LAYER
+#       We need a drop out layer after also!
+# 
+
+class Encoder(nn.Module):
+    def _get_hidden_type(self):
+        raise NotImplementedError
+
     def __init__(self, input_size, hidden_size, num_layers, dropout, dictionary):
-        super(LSTMEncoder, self).__init__()
+        super(Encoder, self).__init__()
         self.hidden_size = hidden_size
         self.dictionary = dictionary
 
         self.embedding = nn.Embedding(input_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=num_layers, dropout=dropout)
+        layer_type = self._get_hidden_type()
+        self.hidden_layers = layer_type(hidden_size, hidden_size, num_layers=num_layers, dropout=dropout)
 
     def forward(self, input, hidden):
         # TODO: make batched
-        embedded = self.embedding(input)
-        embedded = embedded.view(1, 1, -1)
-        output, hidden = self.lstm(embedded, hidden)
-        return output, hidden
+        embedded = self.embedding(input).view(1, 1, -1)
+        output, hidden = self.hidden_layers(embedded, hidden)
+        return output, hidden, None
 
 
-# TODO: add attention
-class LSTMDecoder(nn.Module):
+class Decoder(nn.Module):
+    def _get_hidden_type(self, *args):
+        raise NotImplementedError
+
     def __init__(self, hidden_size, num_layers, dropout, dictionary):
-        super(LSTMDecoder, self).__init__()
+        super(Decoder, self).__init__()
         self.hidden_size = hidden_size
         self.dictionary = dictionary
 
         self.embedding = nn.Embedding(len(dictionary), hidden_size)
-
-        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=num_layers, dropout=dropout)
+        layer_type = self._get_hidden_type()
+        self.hidden_layers = layer_type(hidden_size, hidden_size, num_layers=num_layers, dropout=dropout)
         self.out = nn.Linear(hidden_size, len(dictionary))
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden, encoder_outputs):
         embedded = self.embedding(input).view(1, 1, -1)
-        output, hidden = self.lstm(embedded, hidden)
+        output, hidden = self.hidden_layers(embedded, hidden)
         output = self.softmax(self.out(output[0]))
-        return output, hidden
+        return output, hidden, None
 
 
-class LSTMRNN(nn.Module):
+class RNN(nn.Module):
     # TODO: make dictionary class and write there
     EOS = "EOS"
     BOS = "BOS"
+
+    def _get_encoder(self):
+        raise NotImplementedError
+
+    def _get_decoder(self):
+        raise NotImplementedError
 
     def __init__(self,
         input_size,
@@ -56,15 +73,15 @@ class LSTMRNN(nn.Module):
         src_dictionary,
         tgt_dictionary,
         max_length=64):
-        super(LSTMRNN, self).__init__()
+        super(RNN, self).__init__()
 
         self.input_size = input_size
         self.max_length = 64
         self.num_layers = num_layers
         
         # TODO: is this the same dropout as referenced in the paper?
-        self.encoder = LSTMEncoder(input_size, hidden_size, num_layers, dropout, src_dictionary)
-        self.decoder = LSTMDecoder(hidden_size, num_layers, dropout, tgt_dictionary)
+        self.encoder = self._get_encoder()(input_size, hidden_size, num_layers, dropout, src_dictionary)
+        self.decoder = self._get_decoder()(hidden_size, num_layers, dropout, tgt_dictionary)
 
     def device(self):
         # TODO: consider optimizing
@@ -72,8 +89,9 @@ class LSTMRNN(nn.Module):
 
     def init_hidden(self):
         # TODO: make it work for bszs
-        hidden = self.num_layers * [torch.zeros(2, 1, self.encoder.hidden_size, device=self.device(), requires_grad=True)]
-        return hidden
+        if self.num_layers > 1:
+            return self.num_layers * [torch.zeros(self.num_layers, 1, self.encoder.hidden_size, device=self.device(), requires_grad=True)]
+        return torch.zeros(self.num_layers, 1, self.encoder.hidden_size, device=self.device(), requires_grad=True)
 
     def forward(self, input, target, teacher_forcing=False):
         input_length = input.shape[0]
@@ -87,8 +105,9 @@ class LSTMRNN(nn.Module):
         )
         encoder_hidden = self.init_hidden()
         for idx in range(input_length):
+            
             # Note: No need to loop with torch LSTM, but needed fro GRU?
-            encoder_output, encoder_hidden = self.encoder(
+            encoder_output, encoder_hidden, _ = self.encoder(
                 input[idx],
                 encoder_hidden
             )
@@ -104,7 +123,7 @@ class LSTMRNN(nn.Module):
         decoder_outputs = []
 
         for idx in range(target_length):
-            decoder_output, decoder_hidden = self.decoder(
+            decoder_output, decoder_hidden, _ = self.decoder(
                 decoder_input, decoder_hidden, encoder_outputs
             )
             
@@ -122,38 +141,43 @@ class LSTMRNN(nn.Module):
         return torch.stack(decoder_outputs) 
 
 
-class GRUEncoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, dropout, dictionary):
-        super(GRUEncoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.dictionary = dictionary
-
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size,hidden_size,num_layers,dropout=dropout)
-
-    def forward(self, input, hidden):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output, hidden = self.gru(embedded, hidden)
-        return output, hidden
+# 
+#  LSTM variant below
+#
 
 
-class GRUDecoder(nn.Module):
-    def __init__(self, hidden_size, num_layers, dropout, dictionary):
-        super(GRUDecoder, self).__init__()
-        self.hidden_size = hidden_size
-        self.dictionary = dictionary
+class LSTMEncoder(Encoder):
+    def _get_hidden_type(self):
+        return nn.LSTM
 
-        self.embedding = nn.Embedding(len(dictionary), hidden_size)
 
-        self.gru = nn.GRU(hidden_size,hidden_size,num_layers,dropout=dropout)
-        self.out = nn.Linear(hidden_size, len(dictionary))
-        self.softmax = nn.LogSoftmax(dim=1)
+class LSTMDecoder(Decoder):
+    def _get_hidden_type(self):
+        return nn.LSTM
 
-    def forward(self, input, hidden, encoder_outputs):
-        embedded = self.embedding(input).view(1, 1, -1)
-        output, hidden = self.gru(embedded, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
+
+class LSTMRNN(RNN):
+    def _get_encoder(self):
+        return LSTMEncoder
+
+    def _get_decoder(self):
+        return LSTMDecoder
+
+
+#
+#  GRU variant below
+# 
+
+
+class GRUEncoder(Encoder):
+    def _get_hidden_type(self):
+        return nn.GRU
+
+
+class GRUDecoder(Decoder):
+    def _get_hidden_type(self):
+        return nn.GRU
+
 
 class AttnGRUDecoder(nn.Module):
     def __init__(self,hidden_size, num_layers,dropout,dictionary):
@@ -185,80 +209,14 @@ class AttnGRUDecoder(nn.Module):
         output, hidden = self.gru(output, hidden)
         output = F.log_softmax(self.out(output[0]),dim=1)
         return output, hidden,attn_weights
-        
-        
-        
-        
-        
-        
-        
-class GRURNN(nn.Module):
-
-    EOS = "EOS"
-    BOS = "BOS"
-    
-    def __init__(self,input_size,hidden_size,num_layers,dropout,src_dictionary,tgt_dictionary,max_length=64):
-        super(GRURNN,self).__init__()
-        
-        self.input_size = input_size
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.max_length = 64
-        
-        self.encoder = GRUEncoder(input_size, hidden_size, num_layers, dropout, src_dictionary)
-        self.decoder = AttnGRUDecoder(hidden_size, num_layers, dropout, tgt_dictionary)
-    
-    
-    def device(self):
-        return next(self.encoder.parameters()).device
-    
-    def init_hidden(self):
-        hidden = torch.zeros(1, 1, self.encoder.hidden_size, device=self.device(), requires_grad=True)
-        return hidden
 
 
+class GRURNN(RNN):
+    def _get_encoder(self):
+        return GRUEncoder
 
-    def forward(self, input, target, teacher_forcing = False):
-        
-        input_length = input.shape[0]
-        target_length = target.shape[0]
-    
-        encoder_outputs = torch.zeros(
-            self.max_length,
-            self.encoder.hidden_size,
-            device=self.device()
-        )
-        encoder_hidden = self.init_hidden()
-        for idx in range(input_length):
-            encoder_output, encoder_hidden = self.encoder(
-                input[idx],
-                encoder_hidden
-            )
-            encoder_outputs[idx] = encoder_output.squeeze()
-    
-        decoder_input = torch.tensor(
-            self.decoder.dictionary[self.BOS],
-            device=self.device()
-        )
-        decoder_hidden = encoder_hidden
-    
-        decoder_outputs = []
-    
-        for idx in range(target_length):
-            decoder_output, decoder_hidden, attn_weights = self.decoder(
-                decoder_input, decoder_hidden, encoder_outputs
-            )
-            
-            decoder_outputs.append(decoder_output)
-            if teacher_forcing:
-                decoder_input = target[idx]  # Teacher forcing
-            else:
-                _topv, topi = decoder_output.topk(1)
-                decoder_input = topi.squeeze().clone().detach()  # detach from history as input
-            
-            if decoder_input.item() == self.decoder.dictionary[self.EOS]:
-                break
-        return torch.stack(decoder_outputs) 
+    def _get_decoder(self):
+        return GRUDecoder
 
 
 
@@ -266,18 +224,25 @@ if __name__ == "__main__":
     tasks = "../../data/SCAN/tasks.txt"
     src_dict, tgt_dict = generate_scan_dictionary(tasks, add_bos=True, add_eos=True)
     print("Dictionary loaded")
-    dataset = SCANDataset(tasks, src_dict, tgt_dict)
+    dataset = SCANDataset(tasks, src_dict, tgt_dict, device="cpu")
     print("Dataset loaded")
     model = LSTMRNN(
         len(src_dict),
         100,
-        10,
         2,
         0.5,
         src_dict,
         tgt_dict
     )
     print("LSTM model initialized")
+    input, target = dataset[0]
+    print(f"Input shape {input.shape}. Target shape {target.shape}.")
+    outputs = model.forward(input, target, teacher_forcing=False)
+    print("LSTM model forward ran without teacher forcing")
+    outputs = model.forward(input, target, teacher_forcing=True)
+    print("LSTM model forward ran with teacher forcing")
+
+    print("Starting GRU testing")
     gru_model = GRURNN(
         len(src_dict),
         50,
@@ -287,12 +252,6 @@ if __name__ == "__main__":
         tgt_dict
     )
     print("GRU Model initialized")
-    input, target = dataset[0]
-    print(f"Input shape {input.shape}. Target shape {target.shape}.")
-    outputs = model.forward(input, target, teacher_forcing=False)
-    print("LSTM model forward ran without teacher forcing")
-    outputs = model.forward(input, target, teacher_forcing=True)
-    print("LSTM model forward ran with teacher forcing")
     outputs = gru_model.forward(input, target, teacher_forcing=False)
     print("GRU model forward ran without teacher forcing")
     outputs = gru_model.forward(input, target, teacher_forcing=True)
