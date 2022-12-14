b8653b2e71146bdbe49564379819ab98fb70adff
Vésteinn Snæbjarnarson <vesteinnsnaebjarnarson@gmail.com>
2022-12-14 13:44:52 +0100

    Updated get_stats script for exp2 and exp3

--- stats/get_stats.py
+++ stats/get_stats.py
@@ -11,8 +11,11 @@ def read_log(path):
         final_stats = lines[-1]
         sp_line = final_stats.split()
         acc = float(sp_line[4])
+        oracle_acc = None
+        if "Oracle" in final_stats:
+            oracle_acc = float(sp_line[8])
         stats = json.loads(lines[-2])
-        return {"acc": acc, "stats": stats}
+        return {"acc": acc, "stats": stats, "oracle_acc": oracle_acc}
 
 
 def load_folder(path):
@@ -28,9 +31,9 @@ def load_folder(path):
     return stats
 
 
-def acc(path, pattern=""):
+def acc(path, key="acc", pattern=""):
     stats = load_folder(path)
-    accs = [s["acc"] for s in stats if pattern in s["path"]]
+    accs = [s[key] for s in stats if pattern in s["path"]]
     mean = statistics.mean(accs)
     stdev = statistics.stdev(accs)
     return f"{mean:.1f} ± {stdev:.1f} %", mean, stdev
@@ -38,7 +41,7 @@ def acc(path, pattern=""):
 
 def exp_1_size_splits(path):
     stats = {}
-    for split in [1, 2, 4, 8, 16, 32]:
+    for split in [1, 2, 4, 8, 16, 32, 64]:
         stats[split] = acc(path, pattern=f"_p{split}_")
     return stats
 
@@ -82,7 +85,7 @@ def plot_exp_2(key, xlab, stats, out_file, show=False, legend=[]):
     bars = []
     for idx, stat in enumerate(stats):
         data = stat[key]
-        data = {k: (v[1], v[2]) for k, v in sorted(data.items())}
+        data = {k: (100 * v[1], 100 * v[2]) for k, v in sorted(data.items())}
         height = [d[0] for d in data.values()]
         yerr = [d[1] for d in data.values()]
         if indexes is None:
@@ -105,8 +108,22 @@ def plot_exp_2(key, xlab, stats, out_file, show=False, legend=[]):
     plt.close()
 
 
+def search_error(path):
+    stats = load_folder(path)
+    pred_over_tgt = []
+    for log in stats:
+        for i in range(len(log["stats"]['tgt_probs'])):
+            acc = log["stats"]["accuracy"][i]
+            if acc:
+                # Only for errors
+                continue
+            pred_prob = log["stats"]["pred_probs"][i]
+            tgt_prob = log["stats"]["tgt_probs"][i]
+            pred_over_tgt.append(pred_prob > tgt_prob)
+    return sum(pred_over_tgt) / len(pred_over_tgt)
+
 if __name__ == "__main__":
-    log_dir = "../logs"
+    log_dir = "../logs_8edd2ad1"
 
     #
     # Experiment 1
@@ -131,6 +148,9 @@ if __name__ == "__main__":
 
     exp_2_top = acc(f"{log_dir}/experiment_2")[0]
     exp_2_ob = acc(f"{log_dir}/experiment_2/ob")[0]
+    exp_2_top_oracle = acc(f"{log_dir}/experiment_2", key="oracle_acc")[0]
+    exp_2_ob_oracle = acc(f"{log_dir}/experiment_2/ob", key="oracle_acc")[0]
+
     exp_2_stats_ob = action_command_stats(f"{log_dir}/experiment_2/")
     exp_2_stats_tp = action_command_stats(f"{log_dir}/experiment_2/ob")
     exp_2_stats = [exp_2_stats_ob, exp_2_stats_tp]
@@ -139,8 +159,34 @@ if __name__ == "__main__":
     print(f"Top performing model: {exp_2_top}")
     print(f"Overal best model: {exp_2_ob}")
 
+    print(f"Top performing model w. length oracle: {exp_2_top_oracle}")
+    print(f"Overal best model w. length oracle: {exp_2_ob_oracle}")
+
+
     # Action and command length
     plot_exp_2("command_length", "Command Sequence Length", exp_2_stats, "./exp2_command.png", legend=["TP", "OB"])
     plot_exp_2("action_length", "Action Sequence Length", exp_2_stats, "./exp2_action.png", legend=["TP", "OB"])
 
     # Search error and oracle
+    se_tp = search_error(f"{log_dir}/experiment_2")
+    se_ob = search_error(f"{log_dir}/experiment_2/ob")
+
+    print(f"Percentage prefer generated (top): {100 * se_tp:.2f} %")
+    print(f"Percentage prefer generated (ob): {100 * se_ob:.2f} %")
+
+    
+    #
+    # Experiment 3
+    #
+    exp_3_tl_top = acc(f"{log_dir}/experiment_3/turn_left")[0]
+    exp_3_tl_ob = acc(f"{log_dir}/experiment_3/turn_left/ob")[0]
+    
+    exp_3_j_top = acc(f"{log_dir}/experiment_3/jump")[0]
+    exp_3_j_ob = acc(f"{log_dir}/experiment_3/jump/ob")[0]
+
+    print("--- Experiment 3 ---")
+    print(f"Top performing model (turn left): {exp_3_tl_top}")
+    print(f"Overal best model (turn left): {exp_3_tl_ob}")
+    print(f"Top performing model (jump): {exp_3_j_top}")
+    print(f"Overal best model (jump): {exp_3_j_ob}")
+    
